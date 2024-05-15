8091fedc440b87b13e3817c1db82b1a0c65844ca
Cory Latschkowski <koree@redhat.com>
2024-05-15 11:55:50 -0500

    add: function

--- files/functions.sh
+++ files/functions.sh
@@ -8,3 +8,14 @@ lab_wol(){
     wol $nic
   done
 }
+
+download_fcos(){
+  STREAM=stable
+  VERSION=40.20240416.3.1
+
+  BASEURL=https://builds.coreos.fedoraproject.org/prod/streams/${STREAM}/builds/${VERSION}/x86_64
+
+  curl -LO ${BASEURL}/fedora-coreos-${VERSION}-live-kernel-x86_64
+  curl -LO ${BASEURL}/fedora-coreos-${VERSION}-live-initramfs.x86_64.img
+  curl -LO ${BASEURL}/fedora-coreos-${VERSION}-live-rootfs.x86_64.img
+}
